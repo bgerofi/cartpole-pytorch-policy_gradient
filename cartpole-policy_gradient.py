88c618d61444b4ed95abbf59d216e5f86afff380
Balazs Gerofi <bgerofi@riken.jp>
2018-11-20 21:10:43 +0900

    reorganize for cleaner OOP structure

--- cartpole-policy_gradient.py
+++ cartpole-policy_gradient.py
@@ -13,7 +13,7 @@ env = gym.make('CartPole-v1')
 env.seed(1)
 torch.manual_seed(1)
 
-#Hyperparameters
+# Hyperparameters
 learning_rate = 0.005
 gamma = 0.99
 
@@ -23,12 +23,12 @@ class Policy(nn.Module):
         super(Policy, self).__init__()
         self.state_space = env.observation_space.shape[0]
         self.action_space = env.action_space.n
-        
+
         self.l1 = nn.Linear(self.state_space, 128, bias=False)
         self.l2 = nn.Linear(128, self.action_space, bias=False)
-        
+
         self.gamma = gamma
-        
+
         # Episode policy and reward history 
         self.policy_history = []
         self.reward_episode = []
@@ -36,76 +36,79 @@ class Policy(nn.Module):
         self.reward_history = []
         self.loss_history = []
 
-    def forward(self, x):
-        model = torch.nn.Sequential(
+        self.model = torch.nn.Sequential(
             self.l1,
             nn.Dropout(p=0.6),
             nn.ReLU(),
             self.l2,
             nn.Softmax(dim=-1)
         )
-        return model(x)
+
+        self.optimizer = optim.Adam(self.parameters(), lr=learning_rate)
+        #optimizer = optim.RMSprop(self.parameters(), lr=learning_rate)
+
+
+    def forward(self, x):
+        return self.model(x)
+
+
+    def select_action(self, state):
+        # Select an action (0 or 1) by running policy model and choosing
+        # based on the probabilities in state
+        probs = self.forward(torch.tensor(state, dtype=torch.float32))
+        c = Categorical(probs)
+        action = c.sample()
+
+        self.policy_history.append(c.log_prob(action))
+
+        return action
+
+
+    def update(self):
+        R = 0
+        rewards = []
+
+        # Discount future rewards back to the present using gamma
+        for r in self.reward_episode[::-1]:
+            R = r + self.gamma * R
+            rewards.insert(0,R)
+
+        # Scale rewards
+        rewards = torch.tensor(rewards)
+        rewards = (rewards - rewards.mean()) / (rewards.std())
+
+        # Calculate loss
+        loss = torch.sum(torch.mul(torch.stack(self.policy_history),
+            rewards).mul(-1), -1)
+
+        # Update network weights
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
+
+        # Save and intialize episode history counters
+        self.loss_history.append(loss.item())
+        self.reward_history.append(np.sum(self.reward_episode))
+        self.policy_history = []
+        self.reward_episode= []
 
 
 
 policy = Policy()
-optimizer = optim.Adam(policy.parameters(), lr=learning_rate)
 
-
-def select_action(state):
-    # Select an action (0 or 1) by running policy model and choosing
-    # based on the probabilities in state
-    probs = policy(torch.tensor(state, dtype=torch.float32))
-    c = Categorical(probs)
-    action = c.sample()
-
-    policy.policy_history.append(c.log_prob(action))
-
-    return action
-
-
-def update_policy():
-    R = 0
-    rewards = []
-
-    # Discount future rewards back to the present using gamma
-    for r in policy.reward_episode[::-1]:
-        R = r + policy.gamma * R
-        rewards.insert(0,R)
-
-    # Scale rewards
-    rewards = torch.tensor(rewards)
-    rewards = (rewards - rewards.mean()) / (rewards.std())
-    
-    # Calculate loss
-    loss = torch.sum(torch.mul(
-        torch.stack(policy.policy_history),
-        rewards).mul(-1), -1)
-    
-    # Update network weights
-    optimizer.zero_grad()
-    loss.backward()
-    optimizer.step()
-    
-    # Save and intialize episode history counters
-    policy.loss_history.append(loss.item())
-    policy.reward_history.append(np.sum(policy.reward_episode))
-    policy.policy_history = []
-    policy.reward_episode= []
 
 
 def main(episodes):
     running_reward = 10
-    print("episodes to run: {}".format(episodes))
     for episode in range(episodes):
         state = env.reset() # Reset environment and record the starting state
         done = False
 
         for time in range(1000):
-            if False and episode % 50 == 0:
+            if False and episode % 10 == 0:
                 env.render()
 
-            action = select_action(state)
+            action = policy.select_action(state)
             # Step through environment using chosen action
             state, reward, done, _ = env.step(action.item())
 
@@ -117,7 +120,7 @@ def main(episodes):
         # Used to determine when the environment is solved.
         running_reward = (running_reward * 0.99) + (time * 0.01)
 
-        update_policy()
+        policy.update()
 
         if episode % 10 == 0:
             print('Episode {}\tLast length: {:5d}\tAverage length: {:.2f}'.format(episode, time, running_reward))
